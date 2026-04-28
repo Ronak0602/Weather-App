77bb21b7084f489c35b0af90aabf91ac1fd42ac1
varma Ronak <varmaronak2003@gmail.com>
2026-04-28 16:22:16 +0530

    Weather App

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import { useState } from "react";
+
+function App() {
+  const [city, setCity] = useState("");
+  const [weather, setWeather] = useState(null);
+
+  const fetchWeather = async () => {
+    const response = await fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=aea0faa176bf016333bf6f5c858af575`,
+    );
+    const data = await response.json();
+    setWeather(data);
+  };
+  return (
+    <div>
+      <h1>Weather App</h1>
+      <input
+        value={city}
+        onChange={(e) => setCity(e.target.value)}
+        placeholder="Enter city name"
+      />
+      <button onClick={fetchWeather}>Search</button>
+      {weather && (
+        <div>
+          <h2>{weather.name}</h2>
+          <p>{weather.weather[0].description}</p>
+          <p>{weather.main.temp}°C</p>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default App;
